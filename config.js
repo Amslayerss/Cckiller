6871129604c494a21859b17fb51c7d0d214de9d8
Amslayerss <italycountryhumanss@gmail.com>
2025-04-07 07:05:23 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,22 @@
+// CC KILLER BOT
+
+module.exports = {
+  numberOfRegisterInOneTime: 6,  // CC KILLER BOT - https://t.me/BLACKHATMONKCHAT
+  // no of registration want to make in one command.
+
+  domains: ["lamoshitt.com", "ratvirus.shop"], // CC KILLER BOT
+  // names of domain for the emails.
+
+  oneTimeMailGen: 10 * 100000,
+
+  telegramBotToken: "7788498936:AAGEqg-4Hyo53LDgwFUwe37vSgrFREhCRxg", // CC KILLER BOT
+  // bot token of telegram bot. Create a bot using https://t.me/BotFather then generate the token of the bot.
+
+  adminNamesForTelegramBot: ["notsoamin"], // CC KILLER BOT - https://t.me/BLACKHATMONKCHAT
+  // name of the admin/developer who's name will be displayed when any unknown user use the command of the bot in telegram.
+
+  authorisedUserForTelegramBot: ["notsoamin"], // CC KILLER BOT
+  // username or id of the user who can use the bot.
+};
+
+// CC KILLER BOT
